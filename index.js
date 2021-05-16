6590ca7ef4f54b73d49522f2a8aa0c981180bab1
sbb3391 <sbb33391@gmail.com>
2021-05-16 06:43:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+  updateDOM();
+});
+
+function updateDOM() {
+  document.querySelector("#text").innerHTML = "This is really cool!"
+}
+
